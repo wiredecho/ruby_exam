f4e40ab54e9706d736fc0aca5c0be5e65c672ca1
wiredecho <wiredecho@gmail.com>
2016-07-25 00:09:14 -0700

    first commit, log reg done, User Group

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def main
+  end
+
+  def create
+  	user = User.new(user_params)
+
+  	if user.save
+  		session[:user_id] = user.id
+  		redirect_to "/"
+  	else
+  		flash[:errors] = user.errors.full_messages
+  		redirect_to '/'
+  	end
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+end
